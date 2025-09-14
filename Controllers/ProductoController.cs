59c36a9c1225b9d31dd28ad9fa9d00f295c1e090
Jolfre Gonzalez <jolfre.gonzalez01@gmail.com>
2025-09-13 19:04:39 -0500

    Se termina el crud completo en asp.NETCore

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using MiPrimeraApp.Models;
+using MiPrimeraApp.Data;
+using System.Linq;
 
 namespace MiPrimeraApp.Controllers;
 
 public class ProductoController : Controller
 {
+    private readonly ApplicationDbContext _context;
+
+    public ProductoController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
     public IActionResult Lista()
     {
         var Lista = new List<Producto>{
@@ -13,6 +21,12 @@ public class ProductoController : Controller
             new () { Id = 3, Nombre = "Teclado", Precio = 120}
          };
         return View(Lista);
+    }
+    //READ: Lista de productos
+    public IActionResult Index()
+    {
+        var productos = _context.Productos.ToList();
+        return View(productos);
     }
     [HttpGet]
     public IActionResult Crear()
@@ -30,11 +44,34 @@ public class ProductoController : Controller
         }
         // Aqui normalmente lo guardariamos en la base de datos
         // por ahora lo mostramos en consola
+        _context.Productos.Add(producto);
+        _context.SaveChanges();
         Console.WriteLine($"Producto:{producto.Nombre}, Precio:{producto.Precio}");
-        return RedirectToAction("Confirmacion");
+        return RedirectToAction("Index");
     }
-    public IActionResult Confirmacion()
+    public IActionResult Editar(Producto producto)
+    { 
+        if (!ModelState.IsValid)
+        {
+            //Si hay errores vuelve a mostrar el formulario
+            return View(producto);
+        }
+        // Aqui normalmente lo guardariamos en la base de datos
+        // por ahora lo mostramos en consola
+        _context.Productos.Update(producto);
+        _context.SaveChanges();
+        Console.WriteLine($"Producto:{producto.Nombre}, Precio:{producto.Precio}");
+        return RedirectToAction("Index");
+    }
+    public IActionResult Eliminar(int id)
     {
-        return View();
+        var producto = _context.Productos.Find(id);
+        if (producto == null)
+        {
+            return NotFound();
+        }
+        _context.Productos.Remove(producto);
+        _context.SaveChanges();
+        return RedirectToAction("Index");        
     }
 }
